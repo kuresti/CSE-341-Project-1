f69f394c175414f4f762f076bd8462037a852725
kuresti <55415865+kuresti@users.noreply.github.com>
2024-11-16 12:49:04 -0700

    Update index.js
    -Commented out chrisRoute
    -Added routes.get with swagger tag

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1')
 
+
 /* ******************************
  * Lesson 1 Routes
  * ******************************/
-routes.get('/', lesson1Controller.chrisRoute);
+//routes.get('/', lesson1Controller.chrisRoute);
+routes.get('/', (req, res,) => {
+    //swagger.tags=['Hello world']
+    res.send('Hello World');
+})
 routes.get('/haydon', lesson1Controller.haydonRoute);
 routes.get('/jonathan', lesson1Controller.jonathanRoute);
 
@@ -15,6 +20,7 @@ routes.get('/jonathan', lesson1Controller.jonathanRoute);
  * Use route for contacts CSE-Project-1/contacts
  * ******************************/
 routes.use('/contacts', require('./contacts'));
+routes.use('/', require('./contacts'));
 
 
 module.exports = routes;
