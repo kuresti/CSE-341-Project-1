3244e440ea947fd8e4f62a4f02ee9d4726f5fc4f
kuresti <55415865+kuresti@users.noreply.github.com>
2024-11-15 18:52:09 -0700

    Update contacts.js
    -Added POST route
    -Added PUT route
    -Added DELETE route

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,4 +12,19 @@ router.get('/', contactsCont.getAll);
 
 router.get('/:id', contactsCont.getSingle);
 
+/* *******************************
+ * POST Routes
+ * *******************************/
+router.post('/', contactsCont.createNewContact);
+
+/* *******************************
+ * PUT Routes
+ * *******************************/
+router.put('/:id', contactsCont.updateContact);
+
+/* *******************************
+ * DELETE Routes
+ * *******************************/
+router.delete('/:id', contactsCont.deleteContact);
+
 module.exports = router;
