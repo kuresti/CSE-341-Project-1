d9612947e63099890c73ddd22a172e15105b6ac8
kuresti <55415865+kuresti@users.noreply.github.com>
2024-11-15 18:49:57 -0700

    Update contacts.js
    -Added createNewContact
    -Added updateContact
    -Added deleteContact

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -27,4 +27,60 @@ const getSingle = async(req, res) => {
     });
 };
 
-module.exports = { getAll, getSingle };
+/* ********************************
+ * POST Create New Contact
+ * ********************************/
+const createNewContact = async(req, res) => {
+    const newContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+   const response = await mongodb.getDb().collection('contacts').insertOne(newContact);
+   if (response.acknowledged) {
+    res.status(204).send();
+   } else {
+    res.status(500).json(response.error || 'An error occurred while creating a contact')
+   }
+};
+
+/* ***********************************
+ * PUT Update Contact
+ * ***********************************/
+const updateContact = async(req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().collection('contacts').replaceOne({_id: contactId}, contact);
+    if(response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error occurred while updating the contact.');
+    }
+};
+
+/* ***********************************
+ * DELETE Contact
+ * ***********************************/
+const deleteContact = async(req,res) => {
+   const contactId = new ObjectId(req.params.id);
+   const response = await mongodb.getDb().collection('contacts').deleteOne({ _id: contactId });
+   if (response.deletedCount > 0) {
+    res.status(204).send();
+   } else {
+    res.status(500).json(response.error || 'An error occurred while deleting contact.');
+   }
+};
+ 
+    
+   
+
+
+module.exports = { getAll, getSingle, createNewContact, updateContact, deleteContact };
